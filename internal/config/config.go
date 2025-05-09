951e46d078a63d1221613f8e76bb03c794e361b9
Dmitry Sitnikov <betani.elvin@mail.ru>
2025-05-09 12:28:33 +0300

    Add logger

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,11 @@
 package config
 
-import "time"
+import (
+	"flag"
+	"github.com/ilyakaznacheev/cleanenv"
+	"os"
+	"time"
+)
 
 type Config struct {
 	Env         string        `yaml:"env" env-default:"local"`
@@ -12,4 +17,36 @@ type Config struct {
 type GRPCConfig struct {
 	Port    int           `yaml:"port"`
 	Timeout time.Duration `yaml:"timeout"`
+}
+
+func MustLoad() *Config {
+	path := fetchConfigPath()
+
+	if path == "" {
+		panic("config file path is empty")
+	}
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic("config file not found")
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
+		panic("load config file failed: " + err.Error())
+	}
+	return &cfg
+}
+
+func fetchConfigPath() string {
+	var res string
+
+	flag.StringVar(&res, "config", "", "config path")
+	flag.Parse()
+
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+
+	return res
 }
